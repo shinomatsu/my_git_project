f803824d79f95b947ddf734aa463f3a7674f0c42
shinomatsu <shi968k5@gmail.com>
2017-10-10 23:52:59 +0800

    create datatable for urls

--- git_notes.rb
+++ git_notes.rb
@@ -1,2 +1,12 @@
-def change
+#create datatable
+class CreateUrls < ActiveRecord::Migration[5.0]
+	def change
+		create_table :urls do |t|
+			t.string :original_url
+			t.string :new_url
+
+
+			t.timestamps
+		end
+	end
 end
